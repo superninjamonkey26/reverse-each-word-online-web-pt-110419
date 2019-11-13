a250b1dbb3fdc6e3deb6c73682ef4bb4b4f0369d
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 19:27:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,8 @@
 #puts reverse_each_word.join(" ")
 
 
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|sentence| sentence.reverse!}
+  return reversed_array.join(" ")
+end
