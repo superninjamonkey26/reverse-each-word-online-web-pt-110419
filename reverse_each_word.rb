9975193131347a685526a17d4ae28135436a67f6
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 19:21:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,5 @@ def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
   string.size.times { reversed << split_string.pop }
-  reversed.join
 end
-puts reverse_each_word.join("Hello there, and how are you?")
+puts reverse_each_word.join("")
