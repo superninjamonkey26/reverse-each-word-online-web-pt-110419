c9e37d0d8b93a9226efdcee4d8e1a2139a58d7e4
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 19:29:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
+def reverse_each_word(array)
+  new_array = array.split("")
+  reversed_array = new_array.each {|sentence| sentence.reverse!}
+  return reversed_array.join("")
+
+
 #def reverse_each_word(array)
   #split_array = array.split(" ")
-  #reversed = []
+ 
   #reversed_array = split_array.each {|sentence| sentence.reverse!}
  # string.size.times { reversed << split_string.pop }
  # return reverse_array.join(" ")
@@ -8,8 +14,8 @@
 #puts reverse_each_word.join(" ")
 
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  reversed_array = new_array.each {|sentence| sentence.reverse!}
-  return reversed_array.join(" ")
-end
+#def reverse_each_word(array)
+ # new_array = array.split(" ")
+  #reversed_array = new_array.each {|sentence| sentence.reverse!}
+#  return reversed_array.join(" ")
+#end
