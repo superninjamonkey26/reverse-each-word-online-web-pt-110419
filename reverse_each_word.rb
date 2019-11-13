e2fff8721dcf0cb3ccf1ee8f8c19dbed8c13d728
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 19:20:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,31 +1,3 @@
-#sentence1 = "Hello there, and how are you?"
-#def reverse_each_word(sentence1)
-#  sentence1 = "Hello there, and how are you?"
- # reverse_each_word.map do |sentence1|
-  #  return sentence1
-  #end
-  #return sentence1
-#end
-
-
-#def reverse_each_word(sentence1)
- # sentence = "Hello there, and how are you?"
-  #return sentence.reverse
-#end
-
-
-
-#def reverse_each_word(sentence1)
- # split_sentence1 = sentence1.split("")
-#  reversed = []
- # sentence1.size.times  { reversed << split_sentence1.pop }
-  #reversed.join
- # sentence.each.reverse do |sentence|
-#  end
-#end
-#return reverse_each_word("Hello there, and how are you?")
-
-
 def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
