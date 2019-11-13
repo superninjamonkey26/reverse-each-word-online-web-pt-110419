a68dbaf72cdf55b652522fff7a859065b74e40c3
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 19:24:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
-  split_string = string.split("")
-  reversed = []
-  string.size.times { reversed << split_string.pop }
+  split_string = string.split(" ")
+  #reversed = []
+  reversed_string = split_string.eacg {|sentence| sentence.reverse!}
+ # string.size.times { reversed << split_string.pop }
 end
-puts reverse_each_word.join("")
+puts reverse_each_word.join(" ")
