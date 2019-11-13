bac4bdc236ee55ce111b4237d98f6381eaa11d82
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 19:30:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,18 +5,6 @@ def reverse_each_word(array)
 end
 
 
-#def reverse_each_word(array)
-  #split_array = array.split(" ")
- 
-  #reversed_array = split_array.each {|sentence| sentence.reverse!}
- # string.size.times { reversed << split_string.pop }
- # return reverse_array.join(" ")
-#end
-#puts reverse_each_word.join(" ")
-
-
-#def reverse_each_word(array)
- # new_array = array.split(" ")
-  #reversed_array = new_array.each {|sentence| sentence.reverse!}
+#def reverssentence| sentence.reverse!}
 #  return reversed_array.join(" ")
 #end
